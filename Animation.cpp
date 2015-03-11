0ec7105a9f9adb5e02bf0a018a60555973aa947b
Stefan Rinke <sker65@gmail.com>
2015-03-11 07:45:27 +0100

    - fixed cycling - added handling for small clock - added fsk filter (not working until now)

--- Animation.cpp
+++ Animation.cpp
@@ -16,6 +16,7 @@ sd(sd), panel(panel), clock(clock) {
 	nextAnimationUpdate=0;
 	numberOfAnimations=0;
 	numberOfFrames=0;
+	fskFilter = 18;
 }
 
 boolean Animation::begin() {
@@ -45,36 +46,66 @@ void readString(File& f) {
 }
 
 void Animation::readNextAnimation() {
-	actFilePos = ani.position();
-	readString(ani);
-	// cycles 2
-	cycles = ani.read()*256+ani.read();
-	DPRINTF("cycles: %d\n",cycles);
-	// hold cycles 2
-	holdCycles = ani.read()*256+ani.read();
-	DPRINTF("hold: %d\n", hold);
+	int c = 0;
+	while(true) {
+		actFilePos = ani.position();
 
-	// clock from 2
-	clockFrom = ani.read()*256+ani.read();
-	//clockFrom = 5;
-	DPRINTF("clockFrom: %d\n",clockFrom);
-	// clock small (1 Byte)
-	ani.read();
-	// clock in front (1 Byte)
-	clockInFront = ani.read() != 0;
-	ani.seekCur(4);
-	// clock x / y offset je 2
-	// refresh delay 2
-	refreshDelay = ani.read()*256+ani.read();
-	DPRINTF("refreshDelay: %d\n",refreshDelay);
-	// type (1 Byte)
-	ani.seekCur(1);
-	// 14 Byte insgesamt
-	// frameset count
-	numberOfFrames = ani.read()*256+ani.read();
-	DPRINTF("number of frames: %d\n",numberOfFrames);
-	actFrame = 0;
-	actAnimation++;
+		readString(ani);
+		// cycles 2
+		int cyc = ani.read()*256+ani.read();
+		if( cycles == 0 ) { // beim loop mit mehreren cycles, soll nur
+			cycles = cyc;
+		}
+		DPRINTF("cycles: %d\n",cycles);
+		// hold cycles 2
+		holdCycles = ani.read()*256+ani.read();
+		DPRINTF("hold: %d\n", hold);
+
+		// clock from 2
+		clockFrom = ani.read()*256+ani.read();
+		//clockFrom = 5;
+		DPRINTF("clockFrom: %d\n",clockFrom);
+		// clock small (1 Byte)
+		clockSmall = ani.read()!=0;
+		// clock in front (1 Byte)
+		clockInFront = ani.read() != 0;
+		// clock x / y offset je 2
+		xoffset = ani.read()*256+ani.read();
+		yoffset = ani.read()*256+ani.read();
+		DPRINTF("small: %d, front: %d, x: %d, y: %d\n",
+				clockSmall, clockInFront, xoffset, yoffset );
+		// refresh delay 2
+		refreshDelay = ani.read()*256+ani.read();
+		DPRINTF("refreshDelay: %d\n",refreshDelay);
+		// type (1 Byte)
+		ani.seekCur(1);
+		// 15 Byte insgesamt
+		fsk = ani.read();
+		DPRINTF("fsk: %d\n",fsk);
+		// frameset count
+		numberOfFrames = ani.read()*256+ani.read();
+		DPRINTF("number of frames: %d\n",numberOfFrames);
+		actFrame = 0;
+		actAnimation++;
+		if( fsk <= fskFilter ) break;
+
+		skipAllFrames(ani);
+		if( actAnimation >= numberOfAnimations ) {
+			ani.seek(8); // reset
+			actAnimation = 0;
+		}
+		if( c++ >= numberOfAnimations) break;;
+	}; // at least one must remain
+}
+
+void Animation::skipAllFrames(File& f) {
+	uint16_t len = f.read()*256+f	.read();
+	for( int i = 0; i < numberOfFrames; i++) {
+		DPRINTF("skipping frame: %d\n", i);
+		byte buf[len];
+		f.readBytes(buf, len);
+		f.readBytes(buf, len);
+	}
 }
 
 void Animation::readNextFrame(long now, bool mask) {
@@ -83,10 +114,10 @@ void Animation::readNextFrame(long now, bool mask) {
 		byte buf[buflen];
 		int r = ani.readBytes(buf, buflen);
 		if( mask ) clock.writeTime(now,buf);
-		memmove(panel.getBuffers()[0],buf,buflen);
+		memmove((void*)panel.getBuffers()[0],buf,buflen);
 		r += ani.readBytes(buf, buflen);
 		if( mask ) clock.writeTime(now,buf);
-		memmove(panel.getBuffers()[1],buf,buflen);
+		memmove((void*)panel.getBuffers()[1],buf,buflen);
 		//      Serial.print("reading 2 buffers len: ");Serial.print(buflen);Serial.println(r);
 		//		Serial.print("reading frame: ");Serial.println(actFrame);
 	}
@@ -97,6 +128,8 @@ boolean Animation::update(long now) {
 	if( now> nextAnimationUpdate) {
 		if( hold ) {
 			hold = false;
+			clock.setFont(Clock::Big);
+			panel.clearTime();
 			panel.clear();
 			return true;
 		}
@@ -111,19 +144,26 @@ boolean Animation::update(long now) {
 
 		readNextFrame(now, clockInFront);
 		if( actFrame >= clockFrom ) {
+			if( clockSmall ) {
+				clock.setXoffset(xoffset);
+				clock.setYoffset(yoffset);
+				clock.setFont(Clock::Small);
+			}
+
 			// debug   panel.setPixel(0,0,true);
 			clock.on();
 			// if clock is in front force update
 			clock.update(now);
 			// write with mask / not only with update
+
 		}
 		// todo raus ziehen in den Handler
 		if( actFrame >= numberOfFrames ) { // ende der ani, check auf cycle
-//			if(--cycles>0) {
-//				ani.seek(actFilePos);
-//				actFrame = 0;
-//				actAnimation--;
-//			} else {
+			if(--cycles>0) {
+				ani.seek(actFilePos);
+				actFrame = 0;
+				actAnimation--;
+			} else {
 				// gehe in den Hold status
 				actFrame = 0;
 				// TODO es darf nicht über die lange Zeit gewartet werden
@@ -131,7 +171,7 @@ boolean Animation::update(long now) {
 				// einen update bekommt
 				nextAnimationUpdate = now + 200*holdCycles; // hold
 				hold = true;
-//			}
+			}
 		}
 	}
 	return false;
@@ -140,3 +180,8 @@ boolean Animation::update(long now) {
 Animation::~Animation() {
 }
 
+void Animation::setFskMode(int mode) {
+	uint8_t fsk[] = {18,16,12,6};
+	this->fskFilter = fsk[mode];
+	DPRINTF("setting fsk to %d\n",fskFilter);
+}

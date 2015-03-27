22ffdefd884b1c1ac25ea732155b061ba08275cc
Stefan Rinke <sker65@gmail.com>
2015-03-28 00:19:49 +0100

    - foo

---
+++ PirHandler.cpp
@@ -0,0 +1,32 @@
+/*
+ * PirHandler.cpp
+ *
+ *  Created on: 18.03.2015
+ *      Author: sr
+ */
+
+#include "PirHandler.h"
+#include <WProgram.h>
+
+PirHandler::PirHandler(uint8_t pin) : pin(pin)
+{
+	pinMode(pin, OUTPUT);
+	lastProbe = lastTimeSomebodyWasThere=0;
+	lastResult = false;
+	delay = 45000;
+}
+
+void PirHandler::update(long now) {
+	lastResult = digitalRead(pin);  // true / high means active -> somebody is here
+	lastProbe = now;
+	if( lastResult ) {
+		lastTimeSomebodyWasThere = now;
+	}
+}
+
+PirHandler::~PirHandler() {
+}
+
+bool PirHandler::somebodyHere() {
+	return( lastResult || lastTimeSomebodyWasThere + delay > millis() );
+}
---
+++ PirHandler.h
@@ -0,0 +1,37 @@
+/*
+ * PirHandler.h
+ *
+ *  Created on: 18.03.2015
+ *      Author: sr
+ */
+
+#ifndef PIRHANDLER_H_
+#define PIRHANDLER_H_
+
+#include <stdint.h>
+
+class PirHandler {
+public:
+	PirHandler(uint8_t pin);
+	virtual ~PirHandler();
+
+	void update(long now);
+	bool somebodyHere();
+
+	long getDelay() const {
+		return delay;
+	}
+
+	void setDelay(long delay) {
+		this->delay = delay;
+	}
+
+protected:
+	uint8_t pin;
+	bool lastResult;
+	long lastProbe;
+	long lastTimeSomebodyWasThere;
+	long delay;
+};
+
+#endif /* PIRHANDLER_H_ */

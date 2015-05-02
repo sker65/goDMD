d99c888185522eba11d7497125750613003b2143
Stefan Rinke <sker65@gmail.com>
2015-05-02 15:09:27 +0200

    - externalize ir-codes for final controller

---
+++ ir-codes.h
@@ -0,0 +1,35 @@
+/*
+ * ir-codes.h
+ *
+ *  Created on: 30.04.2015
+ *      Author: sr
+ */
+
+#ifndef IR_CODES_H_
+#define IR_CODES_H_
+
+// header file with codes from the final ir receiver
+
+#define BUT_CH_MINUS 	0xe318261b
+#define BUT_CH 			0x00511dbb
+#define BUT_CH_PLUS 	0xee886d7f
+#define BUT_PREV 		0x52a3d41f
+#define BUT_NEXT 		0xd7e84b1b
+#define BUT_PLAY 		0x20fe4dbb
+#define BUT_MINUS 		0xf076c13b
+#define BUT_PLUS 		0xa3c8eddb
+#define BUT_EQ 			0xe5cfbd7f
+#define BUT_0 			0xc101e57b
+#define BUT_100_PLUS 	0x97483bfb
+#define BUT_200_PLUS 	0xf0c41643
+#define BUT_1 			0x9716be3f
+#define BUT_2 			0x3d9ae3f7
+#define BUT_3 			0x6182021b
+#define BUT_4 			0x8c22657b
+#define BUT_5 			0x488f3cbb
+#define BUT_6 			0x0449e79f
+#define BUT_7 			0x32c6fdf7
+#define BUT_8 			0x1bc0157b
+#define BUT_9 			0x3ec3fc1b
+
+#endif /* IR_CODES_H_ */

592ea00eaa6b54642947c4864bd3a808e3cfa94a
Stefan Rinke <sker65@gmail.com>
2015-05-08 16:59:59 +0200

    - introduced new version.h - inc. version to rc2

---
+++ version.h
@@ -0,0 +1,14 @@
+/*
+ * version.h
+ *
+ *  Created on: 08.05.2015
+ *      Author: sr
+ */
+
+#ifndef VERSION_H_
+#define VERSION_H_
+
+#define VERSION "goDmd v1.01-rc2"
+
+
+#endif /* VERSION_H_ */

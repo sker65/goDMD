81612a3dedd7f7791bdb004cb0c79d823309d2b3
Stefan Rinke <sker65@gmail.com>
2015-08-07 22:14:57 +0200

    - fix ir codes with different receivers

--- ir-codes.h
+++ ir-codes.h
@@ -10,6 +10,7 @@
 
 // header file with codes from the final ir receiver
 
+#if 0
 #define BUT_CH_MINUS 	0x00ffa25d
 #define BUT_CH 			0x00ff629d
 #define BUT_CH_PLUS 	0x00ffe21d
@@ -31,5 +32,30 @@
 #define BUT_7 			0x00ff42bd
 #define BUT_8 			0x00ff4ab5
 #define BUT_9 			0x00ff52ad
+#endif
+
+#if 1
+#define BUT_CH_MINUS 	0xe318261b
+#define BUT_CH 			0x00511dbb
+#define BUT_CH_PLUS 	0xee885d7f
+#define BUT_PREV 		0x52a3d41f
+#define BUT_NEXT 		0xd7e84b1b
+#define BUT_PLAY 		0x20fe4dbb
+#define BUT_MINUS 		0xf076c13b
+#define BUT_PLUS 		0xa3c8eddb
+#define BUT_EQ 			0xe5cfbd7f
+#define BUT_0 			0xc101e57b
+#define BUT_100_PLUS 	0x97483bfb
+#define BUT_200_PLUS 	0xf0c41643
+#define BUT_1 			0x9716be3f
+#define BUT_2 			0x3d9ae3f7
+#define BUT_3 			0x6182021b
+#define BUT_4 			0x8c22657b
+#define BUT_5 			0x488f3cbb
+#define BUT_6 			0x0449e79f
+#define BUT_7 			0x32c6fdf7
+#define BUT_8 			0x1bc0157b
+#define BUT_9 			0x3ec3fc1b
+#endif
 
 #endif /* IR_CODES_H_ */

66f1e67d8a9f1cc95b7457dd751fa9645f3d6b73
Stefan Rinke <sker65@gmail.com>
2015-08-20 02:40:00 +0200

    - use just the right ones

--- ir-codes.h
+++ ir-codes.h
@@ -10,31 +10,8 @@
 
 // header file with codes from the final ir receiver
 
-#if 1
-#define BUT_CH_MINUS 	0x00ffa25d
-#define BUT_CH 			0x00ff629d
-#define BUT_CH_PLUS 	0x00ffe21d
-#define BUT_PREV 		0x00ff22dd
-#define BUT_NEXT 		0x00ff02fd
-#define BUT_PLAY 		0x00ffc23d
-#define BUT_MINUS 		0x00ffe01f
-#define BUT_PLUS 		0x00ffa857
-#define BUT_EQ 			0x00ff906f
-#define BUT_0 			0x00ff6897
-#define BUT_100_PLUS 	0x00ff9867
-#define BUT_200_PLUS 	0x00ffb04f
-#define BUT_1 			0x00ff30cf
-#define BUT_2 			0x00ff1837
-#define BUT_3 			0x00ff7a85
-#define BUT_4 			0x00ff10ef
-#define BUT_5 			0x00ff38c7
-#define BUT_6 			0x00ff5aa5
-#define BUT_7 			0x00ff42bd
-#define BUT_8 			0x00ff4ab5
-#define BUT_9 			0x00ff52ad
-#endif
 
-#if 0
+#if 1
 #define BUT_CH_MINUS 	0xe318261b
 #define BUT_CH 			0x00511dbb
 #define BUT_CH_PLUS 	0xee885d7f

703e558423d45f15ea3566aa5d216db80e6608d7
Stefan Rinke <sker65@gmail.com>
2015-08-20 23:14:48 +0200

    - new debug macros

--- debug.h
+++ debug.h
@@ -10,7 +10,8 @@
 
 #ifdef _DEBUG
 //{ printf( __FILE__ );printf( "\t" );printf( __func__ ); printf("\t"); }
-#define DPRINTF(...)  printf ( __VA_ARGS__ )
+#define DPRINTF2( msg )  printf ( "%s %s:\t" msg, __FILE__, __func__ )
+#define DPRINTF( fmt, ... )  printf ( "%s %s:\t" fmt, __FILE__, __func__, __VA_ARGS__ )
 #define DPRINT(...)  printf ( __VA_ARGS__ )
 
 #else
